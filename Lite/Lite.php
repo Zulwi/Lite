fd787334c626f782f0ecaf8927e303bdfe5fd8fb
JerryLocke <i@Jerrys.me>
2014-06-19 20:52:43 +0800

    兼容低版本

--- Lite/Lite.php
+++ Lite/Lite.php
@@ -2,7 +2,7 @@
 define('START_TIME', microtime(true));
 define('TIMESTAMP', $_SERVER['REQUEST_TIME']);
 
-defined('LITE_PATH') or define('LITE_PATH', __DIR__ . '/');
+defined('LITE_PATH') or define('LITE_PATH', dirname(__FILE__) . '/');
 defined('APP_DEBUG') or define('APP_DEBUG', false);
 defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/App');
 defined('APP_LIB') or define('APP_LIB', APP_PATH . 'Library/');
@@ -12,9 +12,9 @@ defined('APP_VIEW') or define('APP_VIEW', APP_PATH . 'View/');
 defined('APP_COMMON') or define('APP_COMMON', APP_PATH . 'Common/');
 defined('APP_LANG') or define('APP_LANG', APP_PATH . 'Language/');
 
-define('COMMON_PATH', realpath(LITE_PATH . 'Common') . '/');
-define('LIB_PATH', realpath(LITE_PATH . 'Library') . '/');
-define('LANG_PATH', realpath(LITE_PATH . 'Language') . '/');
+define('COMMON_PATH', LITE_PATH . 'Common/');
+define('LIB_PATH', LITE_PATH . 'Library/');
+define('LANG_PATH', LITE_PATH . 'Language/');
 define('LITE_VERSION', '1.0.0 α');
 if (!defined('__ROOT__')) {
 	$root = rtrim(dirname(rtrim($_SERVER['SCRIPT_NAME'], '/')), '/');
