2d15ced5ba2ad6fe0f1186cabad60ed05100eb1b
JerryLocke <i@Jerrys.me>
2014-07-09 10:02:55 +0800

    优化页面

--- App/View/Index/Index.php
+++ App/View/Index/Index.php
@@ -4,41 +4,13 @@
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <title>欢迎使用 Lite</title>
 <style type="text/css">
-* {
-	padding: 0;
-	margin: 0;
-}
-
-body {
-	background: #f1f1f1;
-	font-family: '微软雅黑';
-	color: #333;
-	font-size: 14px;
-}
-
-.tip-box {
-	width: 800px;
-	margin: 150px auto 10px auto;
-	padding: 30px 50px;
-	background: #fff;
-	border: 1 px solid #eaeaea;
-	-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .13);
-	box-shadow: 0 1px 3px rgba(0, 0, 0, .13);
-	-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .13);
-}
-
-.copyright {
-	text-align: center;
-	width: 100%;
-}
-
-a {
-	color: #333;
-}
-
-img {
-	border: 0;
-}
+* { padding: 0; margin: 0; }
+body { background: #f1f1f1; font-family: '微软雅黑'; color: #333; font-size: 14px; }
+.tip-box { width: 800px; margin: 150px auto 10px auto; padding: 30px 50px; background: #fff; border: 1 px solid #eaeaea; -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .13); box-shadow: 0 1px 3px rgba(0, 0, 0, .13); -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .13); }
+a { color: #000; text-decoration: none; }
+h1 { font-size: 32px; line-height: 48px; margin-bottom: 5px; }
+.content { padding-top: 10px; }
+.copyright { padding-top: 12px; color: #999; text-align: center; width: 100%; }
 </style>
 </head>
 <body>
--- Lite/Template/error.php
+++ Lite/Template/error.php
@@ -4,77 +4,26 @@
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <title>系统错误</title>
 <style type="text/css">
-* {
-	padding: 0;
-	margin: 0;
-}
-
-body {
-	background: #f1f1f1;
-	font-family: '微软雅黑';
-	color: #333;
-	font-size: 14px;
-}
-
-.tip-box {
-	width: 800px;
-	margin: 150px auto 10px auto;
-	padding: 30px 50px;
-	background: #fff;
-	border: 1 px solid #eaeaea;
-	-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .13);
-	box-shadow: 0 1px 3px rgba(0, 0, 0, .13);
-	-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .13);
-}
-
-a {
-	color: #000;
-	text-decoration: none;
-}
-
-h1 {
-	font-size: 32px;
-	line-height: 48px;
-	margin-bottom: 5px;
-}
-
-.content {
-	padding-top: 10px
-}
-
-.content .info {
-	margin-bottom: 12px;
-}
-
-.content .info .title {
-	margin-bottom: 3px;
-}
-
-.content .info .title h3 {
-	color: #000;
-	font-weight: 700;
-	font-size: 16px;
-}
-
-.content .info .text {
-	line-height: 24px;
-}
-
-.copyright {
-	padding-top: 12px;
-	color: #999;
-	text-align: center;
-	width: 100%;
-}
+* { padding: 0; margin: 0; }
+body { background: #f1f1f1; font-family: '微软雅黑'; color: #333; font-size: 14px; }
+.tip-box { width: 800px; margin: 150px auto 10px auto; padding: 30px 50px; background: #fff; border: 1 px solid #eaeaea; -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .13); box-shadow: 0 1px 3px rgba(0, 0, 0, .13); -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .13); }
+a { color: #000; text-decoration: none; }
+h1 { font-size: 32px; line-height: 48px; margin-bottom: 5px; }
+.content { padding-top: 10px; }
+.content .info { margin-bottom: 12px; }
+.content .info .title { margin-bottom: 3px; }
+.content .info .title h3 { color: #000; font-weight: 700; font-size: 16px; }
+.content .info .text { line-height: 24px; }
+.copyright { padding-top: 12px; color: #999; text-align: center; width: 100%; }
 </style>
 </head>
 <body>
 	<div class="tip-box">
 		<h1><?php echo strip_tags($e['message']);?></h1>
 		<div class="content">
-			<p><?php if(isset($e['tips'])) echo strip_tags($e['tips']);?></p>
+			<?php if(isset($e['tips'])) echo '<p>'.strip_tags($e['tips']).'</p>';?>
 			<?php if(isset($e['file'])) : ?>
-				<div class="info">
+			<div class="info">
 				<div class="title">
 					<h3>错误位置</h3>
 				</div>
@@ -84,7 +33,7 @@ h1 {
 			</div>
 			<?php endif; ?>
 			<?php if(isset($e['trace'])): ?>
-					<div class="info">
+			<div class="info">
 				<div class="title">
 					<h3>TRACE</h3>
 				</div>
